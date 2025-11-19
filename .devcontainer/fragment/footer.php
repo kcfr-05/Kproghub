037f597e425f841178a5b5595ae19152905109b8
kcfr-05 <15354@ait.nsw.edu.au>
2025-11-19 08:44:17 +0000

    adding php and database

---
+++ .devcontainer/fragment/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<footer class="main-footer">
+        <nav class="social">
+            <a href="https://facebook.com">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://instagram.com">
+                <i class="fa-brands fa-square-instagram"></i>
+            </a>
+            <a href="https://x.com">
+                <i class="fa-brands fa-square-x-twitter"></i>
+            </a>
+            <a href="https://linkedin.com">
+                <i class="fa-brands fa-square-linkedin"></i>
+            </a>
+        </nav>
+        <nav class="footer-nav">
+            <a href="contact.html">Contact</a>
+            <a href="terms.html">Terms</a>
+            <a href="support.html">Support</a>
+            <a href="policies.html">Policies</a>
+        </nav>
+    </footer>
---
+++ .devcontainer/fragment/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Progear Hub</title>
+    <link rel="icon" href="icon.png">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=person,shopping_cart,storefront" />
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <script src="menu.js"></script>
+</head>
